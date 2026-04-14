05e10f891d663192a52ac6a49a7191b215ee5ab5
Ian Felipe <ian.felipe.drunen@gmail.com>
2026-04-13 21:49:12 -0300

    Adicionado projeto SpringBoot

---
+++ Backend/calculeco/src/main/java/com/ecoar/calculeco/CalculecoApplication.java
@@ -0,0 +1,13 @@
+package com.ecoar.calculeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculecoApplication.class, args);
+	}
+
+}
---
+++ Backend/calculeco/src/test/java/com/ecoar/calculeco/CalculecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoar.calculeco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
