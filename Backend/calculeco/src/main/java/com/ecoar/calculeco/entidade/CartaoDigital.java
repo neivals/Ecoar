2d84284d87b33e8b394fe1ed47e2552477555891
Ian Felipe <ian.felipe.drunen@gmail.com>
2026-05-06 22:47:48 -0300

    Adicionadas classes de entidade de inicio

--- Backend/calculeco/src/main/java/com/ecoar/calculeco/entidade/CartaoDigital.java
+++ Backend/calculeco/src/main/java/com/ecoar/calculeco/entidade/CartaoDigital.java
@@ -1,5 +1,22 @@
 package com.ecoar.calculeco.entidade;
 
-public class CartaoDigital {
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@DiscriminatorValue("DIGITAL")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartaoDigital extends MetodoPagamento {
+
+    @Enumerated(EnumType.STRING)
+    private TipoPagamento tipoPagamento;
 }
---
+++ Backend/calculeco/src/main/java/com/ecoar/calculeco/entidade/TipoTransporte.java
@@ -0,0 +1,6 @@
+package com.ecoar.calculeco.entidade;
+
+public enum TipoTransporte {
+    AVIAO,
+    CAMINHAO;
+}
