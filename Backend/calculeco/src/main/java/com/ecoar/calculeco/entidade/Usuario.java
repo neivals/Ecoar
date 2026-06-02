2cb09362b90614a956d2915a5ab1ca4e15cb6c86
RafaCappetta <rafaelchiappettabarboza@gmail.com>
2026-06-02 00:31:27 -0300

    Cria modelo da classe Usuario

---
+++ Backend/calculeco/src/main/java/com/ecoar/calculeco/entidade/Usuario.java
@@ -0,0 +1,48 @@
+package com.ecoar.calculeco.entidade;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    // UserDetails — Spring Security usa esses métodos
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email; // usamos o email como identificador
+    }
+}
