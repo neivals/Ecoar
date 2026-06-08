6c6e955f863da00bbf2c2157b7228977e6603709
Neivals <tnls@cesar.school>
2026-06-08 14:26:19 -0300

    Mudanças para fazer deploy

--- Frontend/calculeco-ui/src/services/authService.js
+++ Frontend/calculeco-ui/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8080";
+const API = import.meta.env.VITE_API_URL;
 
 export async function cadastrar(email, senha, confirmarSenha) {
     const response = await axios.post(`${API}/auth/cadastro`, {
